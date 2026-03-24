a9de51d60eab686161d65e23a0dc50d4b80ec5a7
Andrew.M <mugisaandrew635@gmail.com>
2026-03-24 10:17:22 -0400

    Fix typo in password length check condition

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function generatePassword() {
   for (let i = 0; i < lengthInput; i++) {
     firstHarshed.push(randomChar());
     secondHarshed.push(randomChar());
-    if(firstHarshed.lenght > 100){ showAlert("Max password lenght is 100")}
+    if(firstHarshed.length > 100){ showAlert("Max password lenght is 100")}
   }
 
   updateDisplay();
